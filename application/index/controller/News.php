f1df799c7929ad664bcd785c58b36a0aa93e6cb6
cdmin207078 <cdmin207078@foxmail.com>
2018-01-12 18:10:31 +0800

    foo

---
+++ application/index/controller/News.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+
+class News {
+  public function index() {
+    echo '<h1>index - news page</h1>';
+  }
+
+  public function hello() {
+    echo 'hello - news page';
+  }
+}
